4222a3161ded8209a46f75b05792999b6b1f9490
awesome1238765 <60079834+awesome1238765@users.noreply.github.com>
2024-04-14 17:03:21 -0400

    Update tessalation.py
    Basic shape created. Now time to just duplicate them everywhere.

--- tessalation.py
+++ tessalation.py
@@ -1,3 +1,56 @@
-import turtles
-def tessalation():
-  #Something will go here eventually. 
+import turtle
+screen = turtle.Screen()
+screen.setup(1000,1000)
+screen.tracer(0)
+tessalationTurtle = turtle.Turtle()
+
+def buildBasicShape():
+    for x in range(0, 901):
+        tessalationTurtle.forward(.1)
+        tessalationTurtle.left(0.1)
+    tessalationTurtle.right(180)
+    for x in range(0, 901):
+        tessalationTurtle.forward(0.1)
+        tessalationTurtle.left(0.1)
+    tessalationTurtle.right(60)
+    for x in range(0, 901):
+        tessalationTurtle.forward(0.1)
+        tessalationTurtle.right(0.1)
+    tessalationTurtle.right(180)
+    for x in range(0, 901):
+        tessalationTurtle.forward(0.1)
+        tessalationTurtle.right(0.1)
+    tessalationTurtle.right(60)
+    for x in range(0, 901):
+        tessalationTurtle.forward(0.1)
+        tessalationTurtle.left(0.1)
+    tessalationTurtle.right(180)
+    for x in range(0, 901):
+        tessalationTurtle.forward(0.1)
+        tessalationTurtle.left(0.1)
+    tessalationTurtle.right(60)
+    for x in range(0, 901):
+        tessalationTurtle.forward(0.1)
+        tessalationTurtle.right(0.1)
+    tessalationTurtle.right(180)
+    for x in range(0, 901):
+        tessalationTurtle.forward(0.1)
+        tessalationTurtle.right(0.1)
+    tessalationTurtle.right(60)
+    for x in range(0, 901):
+        tessalationTurtle.forward(0.1)
+        tessalationTurtle.left(0.1)
+    tessalationTurtle.right(180)
+    for x in range(0, 901):
+        tessalationTurtle.forward(0.1)
+        tessalationTurtle.left(0.1)
+    tessalationTurtle.right(60)
+    for x in range(0, 901):
+        tessalationTurtle.forward(0.1)
+        tessalationTurtle.right(0.1)
+    tessalationTurtle.right(180)
+    for x in range(0, 901):
+        tessalationTurtle.forward(0.1)
+        tessalationTurtle.right(0.1)
+
+buildBasicShape()
