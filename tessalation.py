256c815cb83b748d936f923dd5061df78b48b26b
awesome1238765 <60079834+awesome1238765@users.noreply.github.com>
2024-04-16 21:09:52 -0400

    Update tessalation.py
    Close to actually finishing the tessalation. Just have to tune in some values. Good luck.

--- tessalation.py
+++ tessalation.py
@@ -1,3 +1,4 @@
+# **THIS WILL TAKE A LONG TIME TO RUN. I UNDERSTAND THAT YOU MAY THINK ITS CRASHED. IT DIDN'T IT JUST TAKES LIKE 2 MINUTES. THANK YOU FOR YOUR UNDERSTANDING.**
 import turtle
 maxXRange = 1000
 maxYRange = 1000
@@ -8,7 +9,7 @@ tessalationTurtle = turtle.Turtle()
 
 def buildBasicShape():
     for x in range(0, 901):
-        tessalationTurtle.forward(.1)
+        tessalationTurtle.forward(0.1)
         tessalationTurtle.left(0.1)
     tessalationTurtle.right(180)
     for x in range(0, 901):
@@ -55,15 +56,27 @@ def buildBasicShape():
         tessalationTurtle.forward(0.1)
         tessalationTurtle.right(0.1)
 
-def buildColumn(x):
-    for y in range(-1 * maxYRange, maxYRange, 199):
-        tessalationTurtle.setheading(0)
-        tessalationTurtle.penup()
-        tessalationTurtle.goto(x, y)
-        tessalationTurtle.pendown()
-        buildBasicShape()
+def buildColumnOdd():
+    for x in range(-1 * maxXRange, maxXRange, 330): #In order to change how close they are to each other, change the 3rd number (330 if you haven't edited it.) 
+        for y in range(-1 * maxYRange, maxYRange, 199):
+            tessalationTurtle.setheading(0)
+            tessalationTurtle.penup()
+            tessalationTurtle.goto(x, y)
+            tessalationTurtle.pendown()
+            buildBasicShape()
 
-buildColumn(0)
+def buildColumnEven():
+    for x in range(int((-1 * maxXRange) + 165), int(maxXRange - 165), 330): #In order to change how close they are to each other, change the 3rd number (330 if you haven't edited it.) and the + or minus numbers(+ 165 and - 165) (its just 1/2 of the 3rd number)
+        for y in range(int((-1 * maxYRange) - 99.5), int(maxYRange + 140), 199):
+            tessalationTurtle.setheading(0)
+            tessalationTurtle.penup()
+            tessalationTurtle.goto(x, y)
+            tessalationTurtle.pendown()
+            buildBasicShape()
+    
+
+buildColumnOdd()
+buildColumnEven()
 
 
 #This is just to make sure it stays on the screen with VS Code, you may disregard it. 
